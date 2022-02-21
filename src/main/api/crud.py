25f092a079d17cd3317f8de7ba2fc3d7ca65c777
ahmedredahussien <v-areda@expediagroup.com>
2022-02-21 13:04:11 +0100

    Using instance of CommonUtils

--- src/main/api/crud.py
+++ src/main/api/crud.py
@@ -6,18 +6,16 @@ from src.main.api import models, schemas
 
 
 def get_voucher_by_segment(db: Session, segment: str, last_order_ts: str, first_order_ts: str, total_orders: int):
+    common_utils = CommonUtils()
     if segment == "recency_segment":
-        segment_catgeory = CommonUtils.get_recency_segment(CommonUtils,last_order_ts, first_order_ts)
+        segment_catgeory = common_utils.get_recency_segment(last_order_ts, first_order_ts)
         filter_query = (models.Voucher.recency_segment == segment_catgeory) and (models.Voucher.recency_voucher_rank == 1)
         print(segment_catgeory)
         return db.query(models.Voucher).filter(filter_query).first()
     elif segment == "frequent_segment":
-        segment_catgeory = CommonUtils.get_frequent_segment(CommonUtils,total_orders)
+        segment_catgeory = common_utils.get_frequent_segment(total_orders)
         filter_query = (models.Voucher.frequent_segment == segment) and (models.Voucher.frequent_voucher_rank == 1)
         print(segment_catgeory)
         return db.query(models.Voucher).filter(filter_query).first()
 
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
 
